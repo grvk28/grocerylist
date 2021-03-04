b6e367182f915123e111a645fc183a5db11015ad
grvk28 <40272398+grvk28@users.noreply.github.com>
2021-03-04 21:40:39 +0530

    Update settings.py

--- sbrs/settings.py
+++ sbrs/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@g_c+fbb25q--o-@h6rb+e@7pv!-h)@oovtc(fj^cw$8#(7m7f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
